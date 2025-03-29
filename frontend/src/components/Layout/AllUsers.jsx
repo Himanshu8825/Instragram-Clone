638fc00e5c7d56e42c6518cd92daf2853e9a7a8c
Himanshu Verma <kumarsurajverma6001@gmail.com>
2025-03-29 12:42:26 +0530

    Extra

---
+++ frontend/src/components/Layout/AllUsers.jsx
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const AllUsers = () => {
+  const API_BASE_URL = 'https://instragram-clone-yubw.onrender.com/api/v1';
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get(`${API_BASE_URL}/all`);
+        console.log(response);
+
+        setUsers(response.data.users);
+      } catch (err) {
+        console.error('Error fetching users:', err);
+        setError('Failed to load users');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  if (loading) return <p>Loading users...</p>;
+  if (error) return <p>{error}</p>;
+
+  return (
+    <div>
+      <h2>All Users</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user._id}>
+            <strong>{user.username}</strong> - {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default AllUsers;
